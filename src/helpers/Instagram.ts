356fb01438f2676fa1f8831e4d3b06fd4c5d0a8b
Mika Greif <mika@greif.me>
2023-09-23 23:58:33 +0200

    Add: like timeline

--- src/helpers/Instagram.ts
+++ src/helpers/Instagram.ts
@@ -1,6 +1,10 @@
 import {
   AccountRepositoryLoginResponseLogged_in_user,
   IgApiClient,
+  TagFeed,
+  TagFeedResponseItemsItem,
+  TimelineFeed,
+  TimelineFeedResponseMedia_or_ad,
   UserFeedResponseItemsItem,
 } from "instagram-private-api"
 
@@ -17,15 +21,24 @@ export class Instagram {
     this.password = password
   }
 
+  public log(...args: any[]) {
+    console.log(`[${this.username}][debug] -`, ...args)
+  }
+
+  public error(...args: any[]) {
+    console.log(`[${this.username}][error] -`, ...args)
+  }
+
   public async login() {
     this.ig.state.generateDevice(this.username)
-    await this.ig.simulate.preLoginFlow()
-    const loggedInUser = await this.ig.account.login(
-      this.username,
-      this.password,
-    )
-    process.nextTick(async () => await this.ig.simulate.postLoginFlow())
-    this.loggedInUser = loggedInUser
+    const loggedInUser = await this.ig.account
+      .login(this.username, this.password)
+      .then((res) => {
+        this.log("logged in")
+        return res
+      })
+      .catch((err) => this.error(err))
+    this.loggedInUser = loggedInUser || null
   }
 
   public async *getFeed(): AsyncGenerator<UserFeedResponseItemsItem[]> {
@@ -34,7 +47,21 @@ export class Instagram {
     yield await userFeed.items()
   }
 
-  public async likePictures(id: string) {
+  public async getPostsByHashtag(
+    hashtag: string,
+  ): Promise<TagFeedResponseItemsItem[]> {
+    if (!this.loggedInUser) throw new Error("user needs to be logged in")
+    const results: TagFeed = await this.ig.feed.tag(hashtag)
+    return results.items()
+  }
+
+  public async getTimeline(): Promise<TimelineFeedResponseMedia_or_ad[]> {
+    if (!this.loggedInUser) throw new Error("user needs to be logged in")
+    const results: TimelineFeed = await this.ig.feed.timeline()
+    return results.items()
+  }
+
+  public async likePicture(id: string) {
     if (!this.loggedInUser) throw new Error("user needs to be logged in")
     await this.ig.media
       .like({
@@ -46,9 +73,7 @@ export class Instagram {
         },
         d: 1,
       })
-      .then(
-        (res) => console.log("successfully liked media:", id),
-        (err) => console.error("[error] failed to like media: ", err),
-      )
+      .then(() => this.log("liking media:", id))
+      .catch((err) => this.error("[error] failed to like media: ", err))
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,36 @@
-import { IgApiClient, UserFeedResponseItemsItem } from "instagram-private-api"
 import { Instagram } from "./helpers/Instagram"
 
 import dotenv from "dotenv"
 dotenv.config()
 
+const sleep = (millis: number) => new Promise((res) => setTimeout(res, millis))
+
+async function likeTimeline(user: Instagram) {
+  const timeline = await user.getTimeline()
+  user.log(`Starting timeline like for ${timeline.length} pictures`)
+  for (const post of timeline) {
+    await user.likePicture(post.id)
+    await sleep(5000)
+  }
+}
+
 async function main() {
+  let user
   try {
     if (!process.env.USERNAME || !process.env.PASSWORD)
       throw new Error("Specify USERNAME and PASSWORD in env variables")
 
-    const user = new Instagram(process.env.USERNAME, process.env.PASSWORD)
+    user = new Instagram(process.env.USERNAME, process.env.PASSWORD)
     await user.login()
-    const feed: UserFeedResponseItemsItem[] = (await user.getFeed().next())
-      .value
-    for (const post of feed) {
-      await user.likePictures(post.id)
-    }
+    await likeTimeline(user)
   } catch (err) {
-    console.error(err)
+    console.error("Process failed", err)
+  } finally {
+    user &&
+      (await user.ig.account
+        .logout()
+        .catch((err) => console.error("failed to logout user")))
   }
 }
 
-main()
+main().catch((err) => console.log("ERROR", err))
